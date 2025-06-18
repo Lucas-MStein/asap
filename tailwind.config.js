ecb8269c63ef6308b19922f72171e14ca7062f1e
Lucas-Maurice Stein <lms@koempf24.de>
2025-06-18 14:55:23 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./main.js"],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['"Bebas Neue"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
