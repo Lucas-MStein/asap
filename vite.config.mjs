da4fe8214bfac7afc889d63ef4571290b422bed4
Lucas-Maurice Stein <lms@koempf24.de>
2025-06-18 20:47:10 +0200

    added domain

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite'
 import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  base: '/asap/', // Falls dein GitHub Repo so heißt
+  base: '/asap/',
   plugins: [
     viteStaticCopy({
       targets: [
+        { src: 'public/CNAME', dest: '.' },
+        { src: 'style.css', dest: '.' },
         { src: 'initial.html', dest: '.' },
         { src: 'daily.html', dest: '.' },
         { src: 'downloads.html', dest: '.' },
